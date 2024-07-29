8fa233c916a28a6f750ab95fe39a87e84b1c928c
yudonggeun <ydong98@gmail.com>
2024-07-29 11:51:05 +0900

    feat : AuthFilter 구현 - 로그인을 하지 않은 사용자는 게시글 작성을 할 수 없다. -> 게시글 생성 API 필터링 적용

---
+++ src/main/java/woowa/cafe/filter/AuthFilter.java
@@ -0,0 +1,54 @@
+package woowa.cafe.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class AuthFilter implements Filter {
+
+    /**
+     * 로그인이 필요한 API에 대해 로그인 여부를 확인합니다.
+     * 로그인 상태를 확인하기 위해서 세션을 생성하고 세션에 로그인 유저의 정보가 입력되었는지를 확인합니다. 로그인 관련 처리는 {@link woowa.cafe.router.LoginRouter}에서 처리합니다.
+     * @see woowa.cafe.router.LoginRouter#login
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!(request instanceof HttpServletRequest)) {
+            request.getRequestDispatcher("/error/404.html").forward(request, response);
+            return;
+        }
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        for (BlockAPI api : BlockAPI.values()) {
+            if (httpServletRequest.getRequestURI().startsWith(api.url) &&
+                httpServletRequest.getMethod().equals(api.method)) {
+                HttpSession session = httpServletRequest.getSession(false);
+                if (session == null || session.getAttribute("userInfo") == null) {
+                    request.getRequestDispatcher("/login").forward(request, response);
+                    return;
+                }
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private enum BlockAPI {
+        // question
+        QUESTION_CREATE("POST", "/question"),
+        ;
+        private final String method;
+        private final String url;
+
+        BlockAPI(String method, String url) {
+            this.method = method;
+            this.url = url;
+        }
+    }
+
+}
