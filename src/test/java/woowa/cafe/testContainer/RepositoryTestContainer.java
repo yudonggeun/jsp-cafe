bced55c2530030b9301e89e47068690b1cf43932
yudonggeun <ydong98@gmail.com>
2024-08-01 10:50:28 +0900

    refactor : test container 실행 방식 변경 - test container가 테스트 클래스마다 실행되는 것이 아니라 테스트 실행시 단 한번만 컨테이너를 생성하도록 변경하였습니다.

--- src/test/java/woowa/cafe/testContainer/RepositoryTestContainer.java
+++ src/test/java/woowa/cafe/testContainer/RepositoryTestContainer.java
@@ -1,15 +1,15 @@
 package woowa.cafe.testContainer;
 
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public class RepositoryTestContainer {
 
-    @Container
     public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("woowa")
             .withUsername("user1")
             .withPassword("test");
+
+    static {
+        mysql.start();
+    }
 }
